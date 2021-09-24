8486b29c9a343f059b85d95cf083ed893b089fed
Charlie Hoyland <charlie.hoyland@asos.com>
2021-09-24 14:33:32 +0100

    Singleton example

---
+++ Design.Patterns/Singleton/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class DbConnection
+    {
+        private static DbConnection _dbConnection;
+        private readonly Guid _connectionId;
+
+        private DbConnection()
+        {
+            _connectionId = Guid.NewGuid();
+        }
+
+        public static DbConnection CreateConnection()
+        {
+            return _dbConnection ??= new DbConnection();
+        }
+
+        public Guid GetConnectionId()
+        {
+            return _connectionId;
+        }
+    }
+}
